b2f3f8b94ee39028f4bad670992898089a3683f8
elviaandriyani7 <elviaandriani7@gmail.com>
2024-10-24 17:11:26 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 from datetime import datetime
 
-# Mengambil waktu saat ini
-now = datetime.now()
+today = datetime.now()
 
-# Menampilkan waktu dalam format tertentu
-print(f'Sekarang adalah: {now.strftime("%Y-%m-%d %H:%M:%S")}')
+date_time = today.strftime('%Y-%m-%d-%H-%M-%S')
 
+print(date_time)
